d8dda8c19ec597e65abaeb67fa5395a46d669e74
LakshanDerren <isuru21256@gmail.com>
2025-01-22 08:19:19 +0530

    create simple server

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express()
+import dotenv from "dotenv"
+dotenv.config()
+
+
+const PORT = process.env.PORT
+app.listen(PORT,()=>{
+    console.log("server is working");
+    
+})
